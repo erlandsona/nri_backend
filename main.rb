6d66cd041040af3bde7926f9dd946f8370b0899c
Austin Erlandson <austin.erlandson@metova.com>
2016-12-21 19:34:59 -0600

    My take on an okay cli interface for users after some research on other solutions to this problem.

--- main.rb
+++ main.rb
@@ -3,3 +3,18 @@ require 'pry'
 
 
 
+# The program should prompt the user for the number of questions to put in the quiz.
+tries = 3
+questions_for_quiz = 0
+tries.times do |try|
+
+  puts "\nyou have #{3 - try} tries left\n" if try > 0
+  print "Please enter the number of questions to put in the quiz: "
+  # Any integer value greater than 0 is acceptable.
+  questions_for_quiz = gets.to_i
+
+  break if questions_for_quiz.is_a? Integer and questions_for_quiz > 0
+
+  if try >= 2 then abort "\nPlease try regenerating the quiz with an integer greater than zero." end
+  puts "\nWoops, sorry you must enter an integer greater than zero.\n"
+end
